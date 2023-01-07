cc4fd5ec7ac35772450ccdcb3fc29bdb02c3b033
Frosenow <filip.potepa@gmail.com>
2023-01-07 21:38:36 +0100

    created list containg all categories

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,5 +1,31 @@
-let response = window.sessionStorage.getItem('nearbyPlaces')
-response = JSON.parse(response)
-console.log(response)
 
-response.forEach(element => console.log(element.name))
+const places = JSON.parse(window.sessionStorage.getItem('nearbyPlaces'))
+console.log(places)
+
+
+// This function scrap all categories of places nearby and format them  
+function defineCategories(){
+    const categories = []; 
+    places.forEach(place => {
+        place.types.forEach(type => {
+            let typeName = type.toUpperCase().split('_').join(' ')
+            if(!categories.includes(typeName)){
+                categories.push(typeName)
+            }
+        })
+    })
+    return categories
+}
+
+function createHeaders(categories){
+    const ulElement = document.getElementById('places-names')
+    categories.forEach(category => {
+        ulHeaderElement = document.createElement('li')
+        ulHeaderElement.setAttribute('id', String(category))
+        ulHeaderElement.innerText = String(category)
+        ulElement.appendChild(ulHeaderElement)
+    })
+}
+
+const items = defineCategories()
+createHeaders(items)
