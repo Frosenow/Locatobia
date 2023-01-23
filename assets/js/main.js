46b755ec966cd944dece833fa11f98190b4f631a
MrDawkin <mr.dawkin@wp.pl>
2023-01-23 14:46:54 +0100

    Front-panel version 1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,3 +64,18 @@ function callback(results, status, next_page_token){
         // console.log(pageToken)
     }
 }
+
+// RANGE SLIDER 
+
+let slider = document.getElementById('slider')
+let selector = document.getElementById('selector')
+let selectValue = document.getElementById('selectValue')
+let progressBar = document.getElementById('progressBar')
+
+selectValue.innerHTML = slider.value
+
+slider.oninput = function() {
+  selectValue.innerHTML = this.value
+  selector.style.left = this.value + '%'
+  progressBar.style.width = this.value + '%'
+}
